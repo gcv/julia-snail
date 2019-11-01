10fd45471a2a7c940b158a0f5d0536b51eb8c744
Constantine Vetoshev <vetoshev@gmail.com>
2019-11-01 12:49:08 -0700

    Reimplement eval_in_module based on Discourse feedback.

--- snail.jl
+++ snail.jl
@@ -6,23 +6,38 @@ export start, stop
 
 """
 Call eval on expr in the context of the module given by the
-fully_qualified_module_name, which has the form of an array of symbols. The
-first element of the array is always implicitly :Main, and can be omitted.
+fully_qualified_module_name, which has the form of an array of symbols. If the
+first element of the fully-qualified module name is implicitly :Main, then it
+can be omitted.
 
 ```julia
-eval_module_expr([:One, :Two, :Three], :(x = 3 + 5))
+eval_in_module([:One, :Two, :Three], :(x = 3 + 5))
 ```
 is equivalent to
 ```julia
 Main.One.Two.Three.eval(:(x = 3 + 5))
 ```
 """
-function eval_module_expr(fully_qualified_module_name::Array{Symbol, 1}, expr::Expr)
-   fqm = Main.eval(first(fully_qualified_module_name))
-   for m in fully_qualified_module_name[2:end]
-      fqm = fqm.eval(m)
+function eval_in_module(fully_qualified_module_name::Array{Symbol, 1}, expr::Expr)
+   # Retrieving the first module in the chain can be tricky. In general, using
+   # getfield to find a module works, but packages loaded as transitive
+   # dependencies are not necessarily loaded into Main, and so must be found
+   # using another mechanism, i.e., the Base.root_module trick.
+   # https://discourse.julialang.org/t/resolving-a-module-by-its-name/30569/6
+   root = first(fully_qualified_module_name)
+   fqm = try
+      getfield(Main, root)
+   catch err
+      if isa(err, UndefVarError)
+         Base.root_module(Base.__toplevel__, root)
+      else
+         rethrow(err)
+      end
    end
-   return fqm.eval(expr)
+   for m in fully_qualified_module_name[2:end]
+      fqm = getfield(fqm, m)
+   end
+   return fqm.eval(expr) # alternatively: Core.eval(fqm, expr)
 end
 
 running = false
@@ -42,7 +57,7 @@ function start(port=2001)
          @async while Sockets.isopen(client)
             line = readline(client, keep=true)
             in = eval(Meta.parse(line))
-            res = eval_module_expr(in.ns, in.expr)
+            res = eval_in_module(in.ns, in.expr)
          end
       end
       close(server_socket)
