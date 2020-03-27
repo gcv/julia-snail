69014664f421a9d46876ea686ad23a48b70588bb
Constantine Vetoshev <gepardcv@gmail.com>
2020-03-26 22:37:10 -0700

    Update documentation header.

--- julia-snail.el
+++ julia-snail.el
@@ -23,7 +23,7 @@
 
 ;; This package provides an interactive development environment for Julia
 ;; (https://julialang.org/), similar to SLIME for Common Lisp and CIDER for
-;; Clojure. Refer to the README.org file for documentation.
+;; Clojure. Refer to the README.md file for documentation.
 
 ;;; Code:
 
