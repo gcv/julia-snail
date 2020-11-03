e630f33c743aad19e21b8d39ce4d87fa4906d3fa
Constantine Vetoshev <gepardcv@gmail.com>
2020-11-03 08:12:42 -0800

    Remove supposedly unnecessary explicit cl-lib dependency.

--- julia-snail.el
+++ julia-snail.el
@@ -2,7 +2,7 @@
 
 
 ;; URL: https://github.com/gcv/julia-snail
-;; Package-Requires: ((emacs "26.2") (cl-lib "0.5") (dash "2.16.0") (julia-mode "0.3") (s "1.12.0") (spinner "1.7.3") (vterm "0.0.1"))
+;; Package-Requires: ((emacs "26.2") (dash "2.16.0") (julia-mode "0.3") (s "1.12.0") (spinner "1.7.3") (vterm "0.0.1"))
 ;; Version: 1.0.0rc5
 ;; Created: 2019-10-27
 
