cbdfc451b224a7b2675246a93884cddc194d8596
gcv <gepardcv@gmail.com>
2021-12-18 21:07:58 -0800

    Comment out forcecompile.

--- JuliaSnail.jl
+++ JuliaSnail.jl
@@ -570,13 +570,13 @@ end
 
 # XXX: Dirty hackery to improve perceived startup performance follows. Running
 # this function in a separate thread should, in theory, force a bunch of things
-# to JIT-compile in the background before the users notices.
+# to JIT-compile in the background before the user notices.
 # Thank you very much, time-to-first-plot problem!
-function forcecompile()
-   # call these functions before the user does
-   includesin(Base64.base64encode("module Alpha\ninclude(\"a.jl\")\nend"))
-   moduleat(Base64.base64encode("module Alpha\nend"), 1)
-end
+#function forcecompile()
+#   # call these functions before the user does
+#   includesin(Base64.base64encode("module Alpha\ninclude(\"a.jl\")\nend"))
+#   moduleat(Base64.base64encode("module Alpha\nend"), 1)
+#end
 
 end
 
