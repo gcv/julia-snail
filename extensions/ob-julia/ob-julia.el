3dbfc4586546a12acd024a10ecdadd273a1d1854
gcv <gepardcv@gmail.com>
2022-04-17 06:14:56 -0700

    Untabify.

--- extensions/ob-julia/ob-julia.el
+++ extensions/ob-julia/ob-julia.el
@@ -37,40 +37,40 @@
 (defun julia-snail/ob-julia-evaluate (module body src-file out-file)
   (let* ((filename (julia-snail--efn (buffer-file-name (buffer-base-buffer))))
          (line-num 0)
-		 (text (format "JuliaSnail.Extensions.ObJulia.babel_run_and_store(%s, \"%s\", \"%s\", %s)"
-					   module
-					   src-file
-					   out-file
-					   (if julia-snail/ob-julia-use-error-pane "true" "false"))))
+	 (text (format "JuliaSnail.Extensions.ObJulia.babel_run_and_store(%s, \"%s\", \"%s\", %s)"
+		       module
+		       src-file
+		       out-file
+		       (if julia-snail/ob-julia-use-error-pane "true" "false"))))
     ;; This code was meant to startup julia-snail in the org buffer if it's not active, but caused an error
-	;; in org-mode on showing the first result of evalutation. Not sure why.
-	;; (unless (get-buffer julia-snail-repl-buffer)
-	;;   (progn
-	;; 	(julia-snail) t))
-	(julia-snail--send-to-server :Main text)))
+    ;; in org-mode on showing the first result of evalutation. Not sure why.
+    ;; (unless (get-buffer julia-snail-repl-buffer)
+    ;;   (progn
+    ;; 	(julia-snail) t))
+    (julia-snail--send-to-server :Main text)))
 
 ;; This function was adapted from ob-julia-vterm by Shigeaki Nishina (GPL-v3)
 ;; https://github.com/shg/ob-julia-vterm.el as of April 14, 2022
 (defun org-babel-execute:julia (body params)
   (let ((src-file (concat (org-babel-temp-file "julia-src-") ".jl"))
-		(out-file (org-babel-temp-file "julia-out-"))
-		(module (let ((maybe-module (cdr (assq :module params))))
-				  (if maybe-module maybe-module "Main"))))
-	(with-temp-file src-file (insert body))
-	(julia-snail/ob-julia-evaluate module body src-file out-file)
-	(let ((c 0))
+	(out-file (org-babel-temp-file "julia-out-"))
+	(module (let ((maybe-module (cdr (assq :module params))))
+		  (if maybe-module maybe-module "Main"))))
+    (with-temp-file src-file (insert body))
+    (julia-snail/ob-julia-evaluate module body src-file out-file)
+    (let ((c 0))
       (while (and (< c 100) (= 0 (file-attribute-size (file-attributes out-file))))
-		(sit-for 0.1)
-		(setq c (1+ c))))
+	(sit-for 0.1)
+	(setq c (1+ c))))
     (with-temp-buffer
       (insert-file-contents out-file)
       (let ((bs (buffer-string)))
-		(if (catch 'loop
-			  (dolist (line (split-string bs "\n"))
-				(if (> (length line) 12000)
-					(throw 'loop t))))
-			"Output suppressed (line too long)"
-		  bs)))))
+	(if (catch 'loop
+	      (dolist (line (split-string bs "\n"))
+		(if (> (length line) 12000)
+		    (throw 'loop t))))
+	    "Output suppressed (line too long)"
+	  bs)))))
 
 (defun julia-snail/ob-julia-init (repl-buf)
   (julia-snail--send-to-server
